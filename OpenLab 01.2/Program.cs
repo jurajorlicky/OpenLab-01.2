1ae4f50e07e0df606fd5eab9fd820c140b39a496
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:10:42 +0100

    Add project files.

---
+++ OpenLab 01.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OpenLab_01._2
+{
+    class Program
+    {
+        public static int Sum(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put your first number :");
+            var input = Console.ReadLine();
+            Console.WriteLine("Put your second number :");
+            var input2 = Console.ReadLine();
+            int x = Int32.Parse(input);
+            int y = Int32.Parse(input2);
+            int sum = Sum(x, y);
+            Console.WriteLine($"The sum is {sum}.");
+
+        }
+    }
+}
